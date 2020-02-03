74534d673564f7fcbc1a6d7b4212402105116754
Joseph Poser <jposer@release.works>
2020-02-03 11:11:59 +0000

    created more terminal friendly output funtion and added config parsing to allow user specified api key

--- haveibeenpwned-cli/api_interface.py
+++ haveibeenpwned-cli/api_interface.py
@@ -1,9 +1,12 @@
-import requests
-
+import requests, configparser
 class api_interface:
     def __init__(self):
+        self.config = configparser.ConfigParser()
+        self.config.read('config.ini')
+        print(self.config)
+
         self.headers = {'user-agent': 'haveibeenpwned-cli',
-                        'hibp-api-key': '',
+                        'hibp-api-key': '{}'.format(self.config.get('default', 'apiKey'))
         }
 
     def check_pwned(self, email):
@@ -11,8 +14,18 @@ class api_interface:
         search = requests.get(endpoint, headers=self.headers)
         return search.json()
 
+    def list_pwned(self, results):
+        output = ""
+
+        for result in results:
+            output += result.get("Name", "")
+            output += "\n"
+
+        return output
+
 def __init__():
     api = api_interface()
-    print(api.check_pwned("joe.poser@gmail.com"))
+    result = api.check_pwned("joe.poser@gmail.com")
+    print(api.list_pwned(result))
 
 __init__()
